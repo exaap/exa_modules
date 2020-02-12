0d3d687f0d2a1d59dc8ddf8021a7f55bc1a0aa69
joanmarin <joanmarinbustamante@gmail.com>
2020-02-12 16:20:06 -0500

    [ADD] product_manufacturers module and oca_dependecies.txt file

---
+++ product_manufacturers/README.md
@@ -0,0 +1,24 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+======================================
+Homolog Products with his Manufacturer
+======================================
+---------------------
+
+The functionalities implemented are:
+------------------------------------
+* Set additional manufacturers with their associated reference.
+
+Las funcionalidades implementadas son:
+--------------------------------------
+* Establecer Fabricantes adicionales con su referencia asociada.
+
+Credits
+-------
+
+Collaborators
+=============
+
+* Joan Marín <https://github.com/JoanMarin>
---
+++ product_manufacturers/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Homolog Products with his Manufacturer',
+    'version': '8.0.1.0.0',
+    'license': 'AGPL-3',
+    'author': 'Joan Marín Github@JoanMarin',
+    'website': 'https://github.com/exaap/exaap_modules',
+    'category': 'Product',
+    'depends': [
+        'product_manufacturer',
+    ],
+    'summary': 'Relate alternative references to the product',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/product_product.xml',
+        'views/product_template.xml',
+        'views/res_partner.xml',
+    ],
+    'installable': True,
+}
