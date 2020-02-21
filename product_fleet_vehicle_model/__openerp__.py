31049ac6c6b50ff3ce25079eb862fa696dd7f497
joanmarin <joanmarinbustamante@gmail.com>
2020-02-21 02:58:38 -0500

    [REF] modules product_fleet_vehicle_model and product_multi_manufacturer

---
+++ product_fleet_vehicle_model/__openerp__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Fleet Vehicle Model",
+    "version": "8.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "EXA Auto Parts Github@exaap, "
+              "Joan Marín Github@JoanMarin",
+    "website": "https://github.com/exaap/exaap_modules",
+    "category": "Product",
+    "depends": [
+        "product",
+        "fleet",
+    ],
+    "summary": "Fleet Vehicle Models in Products",
+    "data": [
+        "views/product_product.xml",
+        "views/product_template.xml",
+    ],
+    "installable": True,
+}
--- product_multi_manufacturer/models/__init__.py
+++ product_multi_manufacturer/models/__init__.py
@@ -3,6 +3,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import res_partner
-from . import product_manufacturers
+from . import product_manufacturer
 from . import product_product
 from . import product_template
