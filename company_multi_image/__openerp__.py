1e2644275064c15041f5afd72625f3b812c77c83
joanmarin <joanmarinbustamante@gmail.com>
2020-03-10 15:35:13 -0500

    [ADD] company_multi_image module

---
+++ company_multi_image/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Multi images to the company",
+    "version": "9.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "EXA Auto Parts Github@exaap, "
+              "Joan Marín Github@JoanMarin",
+    "website": "https://github.com/exaap/exaap_modules",
+    "category": "Hidden",
+    "depends": [
+        "base",
+    ],
+    "summary": "Multi images to the company",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/res_company_views.xml",
+    ],
+    "installable": True,
+}
