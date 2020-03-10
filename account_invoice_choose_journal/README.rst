378f0c9ef79bdd02200e1892d520c953ae716557
joanmarin <joanmarinbustamante@gmail.com>
2020-03-10 12:42:47 -0500

    [ADD] account_invoice_choose_journal module

---
+++ account_invoice_choose_journal/README.rst
@@ -0,0 +1,17 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+=========================
+Choose Journal in Invoice
+=========================
+
+Independent permission to choose journal in invoice
+
+Credits
+-------
+
+Collaborators
+=============
+
+* Joan Marín <https://github.com/JoanMarin>
---
+++ account_invoice_choose_journal/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Choose Journal in Invoice",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "EXA Auto Parts Github@exaap, "
+              "Joan Marín Github@JoanMarin",
+    "website": "https://github.com/exaap/exaap_modules",
+    "category": "Product",
+    "depends": [
+        "account",
+    ],
+    "summary": "Independent permission to choose journal in invoice",
+    "data": [
+        "security/account_invoice_security.xml",
+        "views/account_invoice_views.xml",
+    ],
+    "installable": True,
+}
