a31b1daa43ed0675bc8c6558e86dd4fc038a0f30
joanmarin <joanmarinbustamante@gmail.com>
2020-03-12 14:41:27 -0500

    [ADD] product_multi_manufacturer module

---
+++ product_multi_manufacturer/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Homolog Products with his Manufacturer",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "EXA Auto Parts Github@exaap, "
+              "Joan Marín Github@JoanMarin",
+    "website": "https://github.com/exaap/exaap_modules",
+    "category": "Product",
+    "depends": [
+        "product_manufacturer",
+    ],
+    "summary": "Relate alternative references to the product",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/product_product_views.xml",
+        "views/product_template_views.xml",
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+}
---
+++ product_multi_manufacturer/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import res_partner
+from . import product_manufacturer
+from . import product_product
+from . import product_template
