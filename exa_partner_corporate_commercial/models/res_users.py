e2837a6df02c883011acde1d97711304ffa07db7
joanmarin <joanmarinbustamante@gmail.com>
2020-07-13 15:52:45 -0500

    [ADD] exa_partner_corporate_commercial module

---
+++ exa_partner_corporate_commercial/models/res_users.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Juan Sebastian Ocampo Ospina <Github@Capriatto>
+# Copyright 2020 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ResUsers(models.Model):
+    _inherit = "res.users"
+
+    is_corporate_commercial = fields.Boolean(string='Corporate Commercial?')
