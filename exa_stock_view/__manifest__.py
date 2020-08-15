bf3e2fe623542b12a960665e70cfdfb4159d081e
joanmarin <joanmarinbustamante@gmail.com>
2020-08-14 23:36:11 -0500

    [ADD] exa_stock_view module

---
+++ exa_stock_view/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Juan Sebastian Ocampo Ospina <Github@Capriatto>
+# Copyright 2020 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "EXA Stock View",
+    "author": "EXA Auto Parts Github@exaap, "
+              "Joan Marín Github@JoanMarin",
+    "license": "AGPL-3",
+    "category": "EXA Modules",
+    "version": "10.0.1.0.0",
+    "website": "https://github.com/exaap/exaap_modules",
+    "installable": True,
+    "images": [],
+    "depends": [
+        "exa_base_modules",
+        "delivery",
+        "base_location_zones"
+    ],
+    "data": [
+        "views/stock_picking_views.xml",
+    ],
+}
