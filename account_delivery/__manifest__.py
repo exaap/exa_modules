d69cae5f8a5fa3a7acb813800b3e9c75f979ad6c
joanmarin <joanmarinbustamante@gmail.com>
2020-08-18 18:22:01 -0500

    [ADD] account_delivery module

---
+++ account_delivery/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Delivery",
+    "author": "EXA Auto Parts Github@exaap, "
+              "Joan Marín Github@JoanMarin",
+    "license": "AGPL-3",
+    "category": "EXA Modules",
+    "version": "10.0.1.0.0",
+    "website": "https://github.com/exaap/exaap_modules",
+    "installable": True,
+    "images": [],
+    "depends": [
+        "account",
+        "delivery",
+    ],
+    "data": [
+        "views/account_invoice_views.xml",
+    ],
+}
---
+++ account_delivery/models/account_invoice.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class AccountInvoice(models.Model):
+    _inherit = 'account.invoice'
+
+    carrier_id = fields.Many2one(
+        comodel_name="delivery.carrier",
+        string="Delivery Method")
