659139fe422363c57aa40ba3f4667f6eca3726de
Capriatto <michu-1@hotmail.com>
2020-09-19 10:23:23 -0500

    [ADD] sale_team_monthly_goal -C

---
+++ sale_team_monthly_goal/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "sale_team_monthly_goal",
+
+    'summary': """
+        This module add a monthly goal for the sale_team           
+    """,
+
+    'description': """
+        This module add a monthly goal for the sale_team
+    """,
+
+    'author': "EXA Auto Parts S.A.S & Solutions",
+    'website': "https://www.exaap.com",
+
+    'category': 'Addon',
+    'version': '10.0.0.2',
+
+    'depends': ['crm'],
+
+    'data': [
+        #'security/ir.model.access.csv',
+        'views/crm_team_form_view.xml',
+    ],
+}
