e270cc04a96d0edc897d4ebccc6b7afceb251231
Alejandro Olano <alejocode@gmail.com>
2020-11-19 15:49:55 -0500

    [FIX] Group User

--- exa_product_application/__manifest__.py
+++ exa_product_application/__manifest__.py
@@ -1,7 +1,13 @@
 {
-    "name": "Produc Application ",
-    "version": "1.0",
-    "author": "Alejandro Olano",
+    "name":
+    "Produc Application ",
+    "version":
+    "1.0",
+    "author":
+    "Alejandro Olano",
     "depends": ['sale'],
-    "data": ['security/ir.model.access.csv', 'views/product_template.xml']
+    "data": [
+        'security/groups.xml', 'security/ir.model.access.csv',
+        'views/product_template.xml'
+    ]
 }
