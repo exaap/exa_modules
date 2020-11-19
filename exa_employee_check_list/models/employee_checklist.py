30c51861dc504bb2b94b6afe92da6755e057edd1
Alejandro Olano <alejocode@gmail.com>
2020-11-19 10:32:52 -0500

    [FIX] Check List Recursive Department

--- exa_employee_check_list/models/employee_checklist.py
+++ exa_employee_check_list/models/employee_checklist.py
@@ -4,5 +4,5 @@ from odoo import models, fields, api
 class EmployeeChecklistInherit(models.Model):
     _inherit = "employee.checklist"
 
-    department_id = fields.Many2one(comodel_name="hr.department",
+    department_id = fields.Many2many(comodel_name="hr.department",
                                     string="Departamento")
--- exa_employee_check_list/models/hr_department.py
+++ exa_employee_check_list/models/hr_department.py
@@ -4,7 +4,6 @@ from odoo import models, fields, api
 class HrDepartmentInherit(models.Model):
     _inherit = "hr.department"
 
-    employee_checklist_id = fields.One2many(comodel_name="employee.checklist",
-                                            inverse_name="department_id",
-                                            string="Check List")
-
+    employee_checklist_id = fields.Many2many(comodel_name="employee.checklist",
+                                             inverse_name="department_id",
+                                             string="Check List")
