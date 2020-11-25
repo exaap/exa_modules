7a78bc979b70c36d29b14c57f8704b2347b44743
Alejandro Olano <alejocode@gmail.com>
2020-11-25 16:21:28 -0500

    [FIX] Funtions Checklist

--- exa_employee_check_list/models/hr_employee.py
+++ exa_employee_check_list/models/hr_employee.py
@@ -1,45 +1,46 @@
 from odoo import models, fields, api
 
 
-class EmployeeMasterInherit(models.Model):
+class HrEmployee(models.Model):
     _inherit = 'hr.employee'
 
-    entry_checklist = fields.Many2many(domain=[
-        '&', ('document_type', '=',
-              'entry'), ('department_id', '=', 'department_id')
-    ])
-    exit_checklist = fields.Many2many(domain=[
-        '&', ('document_type', '=',
-              'exit'), ('department_id', '=', 'department_id')
-    ])
-
     @api.depends('exit_checklist')
-    def exit_progress_in(self):
+    def exit_progress(self):
         for each in self:
             total_len = self.env['employee.checklist'].search_count([
-                '&', ('document_type', '=', 'exit'),
-                ('department_id', '=', self.department_id.id)
-            ])
-            entry_len = len(each.exit_checklist)
+                '&',
+                ('document_type', '=', 'exit'),
+                ('department_id', '=', each.department_id.id)])
+            exit_len = 0
+
+            if each.exit_checklist:
+                exit_checklist = self.env['employee.checklist'].search([
+                '&',
+                ('id', 'in', each.exit_checklist.ids),
+                ('department_id', '=', each.department_id.id)])
+                exit_len = len(exit_checklist)
+
             if total_len != 0:
-                each.exit_progress = (entry_len * 100) / total_len
+                each.exit_progress = (exit_len * 100) / total_len
 
     @api.depends('entry_checklist')
-    def entry_progress_in(self):
+    def entry_progress(self):
         for each in self:
             total_len = self.env['employee.checklist'].search_count([
-                '&', ('document_type', '=', 'entry'),
-                ('department_id', '=', self.department_id.id)
-            ])
-            entry_len = len(each.entry_checklist)
+                '&',
+                ('document_type', '=', 'entry'),
+                ('department_id', '=', each.department_id.id)])
+            entry_len = 0
+
+            if each.entry_checklist:
+                entry_checklist = self.env['employee.checklist'].search([
+                '&',
+                ('id', 'in', each.entry_checklist.ids),
+                ('department_id', '=', each.department_id.id)])
+                entry_len = len(entry_checklist)
+
             if total_len != 0:
                 each.entry_progress = (entry_len * 100) / total_len
 
-    entry_progress = fields.Float(compute=entry_progress_in,
-                                  string='Entry Progress',
-                                  store=True,
-                                  default=0.0)
-    exit_progress = fields.Float(compute=exit_progress_in,
-                                 string='Exit Progress',
-                                 store=True,
-                                 default=0.0)
+    entry_progress = fields.Float(compute=entry_progress, string='Entry Progress', store=True, default=0.0)
+    exit_progress = fields.Float(compute=exit_progress, string='Exit Progress', store=True, default=0.0)
