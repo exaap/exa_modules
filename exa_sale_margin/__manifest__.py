389a363abf42a7f6f602d3ba72df45c0efbdba25
Alejandro Olano <alejocode@gmail.com>
2020-11-25 16:22:14 -0500

    [ADD] Exa Sale Order Margin Percent

---
+++ exa_sale_margin/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 PESOL (http://pesol.es)
+#                Luis Adan Jimenez Hernandez (luis.jimenez@pesol.es)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": " Exa Sale Order Margin Percent",
+    "summary": "Show Percent in sale order",
+    "version": "10.0.1.0.0",
+    "category": "Sales",
+    "author": "Alejandro Olano",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ['sale_order_margin_percent'],
+    "data": [
+        'views/sale_margin_percent_views.xml',
+    ]
+}
