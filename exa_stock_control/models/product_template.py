a210663a8c4a025f0ba04b1b2508a00d9c83107c
Alejandro Olano <alejocode@gmail.com>
2020-12-04 12:09:32 -0500

    [FIX] Security Groups Position Product

--- exa_stock_control/models/product_template.py
+++ exa_stock_control/models/product_template.py
@@ -12,9 +12,26 @@ class ProductTemplate(models.Model):
     position_product = fields.Char(string="Position in main winery",
                                    compute="_position_name")
 
+    edit_position_fields = fields.Boolean(
+        string="Edit 'Positions Fields' Field",
+        compute='_get_edit_positions_fields',
+        store=False)
+
     @api.depends('initial_position', 'final_position')
     def _position_name(self):
         for record in self:
             record.position_product = (record.initial_position
                                        or '') + '  --->  ' + (
                                            record.final_position or '')
+
+    @api.multi
+    def _get_edit_positions_fields(self):
+        user = self.env['res.users'].search([('id', '=', self.env.user.id)])
+        edit_position_fields = False
+
+        if user.has_group(
+                'exa_stock_control.exa_stock_control_group_manager_position'):
+            edit_position_fields = True
+
+        for products in self:
+            products.edit_position_fields = edit_position_fields
--- exa_stock_control/models/stock_inventory.py
+++ exa_stock_control/models/stock_inventory.py
@@ -120,3 +120,19 @@ class InventoryLine(models.Model):
                            related='product_id.position_product')
     product_brand_id = fields.Many2one(string="Product brand",
                                        related="product_id.product_brand_id")
+    edit_position_fields = fields.Boolean(
+        string="Edit 'Positions Fields' Field",
+        compute='_get_edit_positions_fields',
+        store=False)
+
+    @api.multi
+    def _get_edit_positions_fields(self):
+        user = self.env['res.users'].search([('id', '=', self.env.user.id)])
+        edit_position_fields = False
+
+        if user.has_group(
+                'exa_stock_control.exa_stock_control_group_manager_position'):
+            edit_position_fields = True
+
+        for products in self:
+            products.edit_position_fields = edit_position_fields
