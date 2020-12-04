1970dc4372c4a2d190f38aa02edd45e8f0c4e02a
Alejandro Olano <alejocode@gmail.com>
2020-12-04 08:39:50 -0500

    [FIX] Translate es.po

--- exa_product_application/__manifest__.py
+++ exa_product_application/__manifest__.py
@@ -1,10 +1,17 @@
+# -*- coding: utf-8 -*-
 {
     "name":
-    "Produc Application ",
-    "version":
-    "1.0",
+    "Exa Product Application",
+    "license":
+    "AGPL-3",
     "author":
-    "Alejandro Olano",
+    "EXA Auto Parts Github@exaap, Alejandro Olano Github@alejo-code",
+    "category":
+    "EXA Modules",
+    "version":
+    "10.0.1.0.0",
+    "website":
+    "https://github.com/exaap/exaap_modules",
     "depends": ['sale'],
     "data": [
         'security/groups.xml', 'security/ir.model.access.csv',
--- exa_product_application/models/product_application.py
+++ exa_product_application/models/product_application.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from odoo import models, fields
 
 
@@ -7,3 +8,14 @@ class ProductApplications(models.Model):
     name = fields.Char(string="Product Application")
     product_ids = fields.Many2many(comodel_name="product.template",
                                    string="Products")
+
+    product_application_type = fields.Many2many(
+        comodel_name="product.application.type",
+        string="Product application type")
+
+
+class ProductApplicationsType(models.Model):
+    _name = "product.application.type"
+
+    name = fields.Char(string="Product application type")
+    reference_type = fields.Integer(string="Reference")
