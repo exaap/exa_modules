20f58b6b4c77652d3926f8fe0688583173abf1db
Alejandro Olano <alejocode@gmail.com>
2020-12-07 13:04:22 -0500

    [FIX] Product Application Template

--- exa_product_application/models/product_template.py
+++ exa_product_application/models/product_template.py
@@ -6,9 +6,11 @@ class ProductTemplate(models.Model):
     _inherit = "product.template"
 
     apllication_ids = fields.Many2many(comodel_name="product.application",
-                                       string="Product Application")
+                                       string="Product Application",
+                                       required=True)
 
     apllication_type_ids = fields.Many2many(
         comodel_name="product.application.type",
         related="apllication_ids.product_application_type",
-        string="Product application type")
+        string="Product application type",
+        required=True)
