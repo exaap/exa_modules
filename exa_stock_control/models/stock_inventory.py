888f6bebe28b5ac573129f9b086aa7bdb1528cb4
Alejandro Olano <alejocode@gmail.com>
2020-12-07 14:58:14 -0500

    [FIX] Filter All Products Date

--- exa_stock_control/models/stock_inventory.py
+++ exa_stock_control/models/stock_inventory.py
@@ -21,6 +21,74 @@ class InventoryInherit(models.Model):
 
     @api.multi
     def _get_inventory_lines_values(self):
+        if self.filter == 'none':
+            # TDE CLEANME: is sql really necessary ? I don't think so
+            locations = self.env['stock.location'].search([
+                ('id', 'child_of', [self.location_id.id])
+            ])
+            domain = ' stock_quant.location_id in %s AND product_product.active = TRUE'
+            args = (tuple(locations.ids), )
+
+            vals = []
+            Product = self.env['product.product']
+            # Empty recordset of products available in stock_quants
+            quant_products = self.env['product.product']
+            # Empty recordset of products to filter
+            products_to_filter = self.env['product.product']
+            all_products = Product.search([('active', '=', True)])
+            domain += ' AND product_id = ANY (%s)'
+            inventory_lines = self.env["stock.inventory.line"].search([
+                '&', '&', ('inventory_id.date', '>', self.adjustment_date),
+                ('inventory_id.state', '=', 'done'),
+                ('product_id', 'in', all_products.ids)
+            ])
+            adjustment_products = []
+
+            for inventory_line in inventory_lines:
+                adjustment_products.append(inventory_line.product_id.id)
+
+            all_products = Product.search([('id', 'not in',
+                                            adjustment_products)])
+            products_to_filter |= all_products
+            args += (all_products.ids, )
+            self.env.cr.execute(
+                """SELECT product_id, sum(qty) as product_qty, stock_quant.location_id, lot_id as prod_lot_id, package_id, owner_id as partner_id 
+                FROM stock_quant 
+                LEFT JOIN product_product 
+                ON product_product.id = stock_quant.product_id 
+                LEFT JOIN product_template 
+                ON product_product.product_tmpl_id = product_template.id 
+                WHERE %s 
+                GROUP BY product_id, stock_quant.location_id, lot_id, package_id, partner_id """
+                % domain, args)
+
+            for product_data in self.env.cr.dictfetchall():
+                # replace the None the dictionary by False, because falsy values are tested later on
+                for void_field in [
+                        item[0] for item in product_data.items()
+                        if item[1] is None
+                ]:
+                    product_data[void_field] = False
+                product_data['theoretical_qty'] = product_data['product_qty']
+                if product_data['product_id']:
+                    product_data['product_uom_id'] = Product.browse(
+                        product_data['product_id']).uom_id.id
+                    quant_products |= Product.browse(
+                        product_data['product_id'])
+                vals.append(product_data)
+
+            if self.exhausted and products_to_filter:
+                exhausted_vals = self._get_exhausted_inventory_line(
+                    products_to_filter, quant_products)
+                vals.extend(exhausted_vals)
+
+            if vals:
+                return vals
+            else:
+                raise UserError(
+                    _('There are no products available on the selected date %s'
+                      ) % self.adjustment_date)
+
         if self.product_brand_id:
             # TDE CLEANME: is sql really necessary ? I don't think so
             locations = self.env['stock.location'].search([
