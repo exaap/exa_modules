ada0350a88a5be09d85f51e458c68e0690c7d9ae
joanmarin <joanmarinbustamante@gmail.com>
2020-12-09 15:39:57 -0500

    [ADD] delivery_volume_precision module

---
+++ delivery_volume_precision/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Delivery Volume Precision",
+    "version": "10.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "EXA Auto Parts Github@exaap, "
+              "Joan Marín Github@JoanMarin",
+    "website": "https://github.com/exaap/exaap_modules",
+    "category": "Stock",
+    "depends": [
+        "delivery",
+        "product_volume_precision",
+    ],
+    "data": [],
+    "installable": True,
+}
