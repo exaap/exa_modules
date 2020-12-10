f1d69f2ad20cdc8adf70b697528dfa91f0645615
Alejandro Olano <alejocode@gmail.com>
2020-12-10 15:24:26 -0500

    [DEL] Exa Product Product Application

--- exa_product_application/models/product_application.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields
-
-
-class ProductApplications(models.Model):
-    _name = "product.application"
-
-    name = fields.Char(string="Product Application")
-    product_ids = fields.Many2many(comodel_name="product.template",
-                                   string="Products")
