b0ed00e82e0d040aa8b11034389ae0ead339c0c1
Alejandro <alejocode@gmail.com>
2020-12-14 14:45:21 -0500

    [ADD] Product Application

---
+++ product_application/models/product_application.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import models, fields
+
+
+class ProductApplications(models.Model):
+    _name = "product.application"
+    name = fields.Char(string="Product Application",
+                       translate=True,
+                       required=True)
---
+++ product_application/models/product_template.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import models, fields, api
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.template"
+
+    product_apllication_id = fields.Many2one(
+        comodel_name="product.application", string="Application")
+    application_category_id = fields.Many2one(
+        comodel_name="product.application.category",
+        string="Category",
+        domain=[('parent_id', '=', False)])
+    application_sub_category_id = fields.Many2one(
+        comodel_name="product.application.category", string="Sub Categories")
+    application_segment_id = fields.Many2one(
+        comodel_name="product.application.category", string="Segment")
+
+    edit_applications_fields = fields.Boolean(
+        string="Edit 'Application Fields' Field",
+        compute='_get_edit_applications_field',
+        store=False)
+
+    @api.multi
+    def _get_edit_applications_field(self):
+        user = self.env['res.users'].search([('id', '=', self.env.user.id)])
+        edit_applications_fields = False
+
+        if user.has_group(
+                'product_application.product_application_group_manager'):
+            edit_applications_fields = True
+
+        for product in self:
+            product.edit_applications_fields = edit_applications_fields
