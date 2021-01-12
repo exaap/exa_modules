7f93a2caf67f9aba363518f788b5e16aabf4e5e9
Alejandro Olano <alejocode@gmail.com>
2021-01-12 10:22:31 -0500

    [ADD] Product Brand Prefix

--- product_brand_prefix/models/product_template.py
+++ product_brand_prefix/models/product_template.py
@@ -8,7 +8,7 @@ from odoo.exceptions import UserError
 class ProductTemplate(models.Model):
     _inherit = "product.template"
 
-    brand_prefix = fields.Char(related="product_brand_id.prefix")
+    brand_prefix = fields.Char()
 
     @api.model
     def create(self, vals):
@@ -18,34 +18,39 @@ class ProductTemplate(models.Model):
             brand_prefix = self.env["product.brand"].search([
                 ('id', '=', vals.get('product_brand_id'))
             ]).prefix
-            related_vals['default_code'] = (brand_prefix or
-                                            '') + ' - ' + vals['default_code']
+            related_vals['default_code'] = (brand_prefix
+                                            or '') + vals['default_code']
+            related_vals['brand_prefix'] = brand_prefix
         if related_vals:
             template.write(related_vals)
         return template
 
     @api.multi
     def write(self, vals):
-        if vals.get("product_brand_id") or vals.get("brand_prefix"):
+        if vals.get("product_brand_id"):
             brand_prefix = self.env["product.brand"].search([
-                ("id", "=", vals.get("product_brand_id"))
-            ]).prefix
+                ('id', '=', vals.get("product_brand_id"))
+            ])
             product = self.env["product.template"].search([('id', '=', self.id)
-                                                           ]).default_code
-            if brand_prefix is False:
+                                                           ])
+            if self.product_brand_id.prefix is False:
                 raise UserError(_("The brand does not have a prefix assigned"))
-            code = product
-            prefix = code.find('-', 3)
-            if prefix == -1:
-                vals['default_code'] = (brand_prefix
-                                        or '') + '-' + self.default_code
-            if prefix == 3:
-                default_code = self.default_code
-                default_code.replace('-', '')
-                default_code_2 = default_code.replace('-', '')
-                defaultcode2 = default_code_2[3:]
-                defaultcode2.replace('-', '')
-                vals['default_code'] = (brand_prefix
-                                        or '') + '-' + defaultcode2
+            code = product.default_code
+            prefix_code = product.brand_prefix
+            brand_prefix_code = ""
+            if prefix_code is False:
+                brand_prefix_code = (brand_prefix.prefix
+                                     or '') + self.default_code
+                vals["brand_prefix"] = brand_prefix.prefix
+            else:
+                prefix = code.find(prefix_code)
+                if prefix >= 0:
+                    default_code = self.default_code
+                    defaultcode2 = default_code[3:]
+                    brand_prefix_code = (brand_prefix.prefix
+                                         or '') + defaultcode2
+                    vals["brand_prefix"] = brand_prefix.prefix
+
+            vals['default_code'] = brand_prefix_code
 
         return super(ProductTemplate, self).write(vals)
