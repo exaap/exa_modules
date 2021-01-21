1bfffa76ebf6e8e646eb8c3604dfac7ab9bbcbc2
Alejandro Olano <alejocode@gmail.com>
2021-01-21 09:29:05 -0500

    [REF] Rename Module

--- product_category_archive_security/__manifest__.py
+++ product_category_archive_security/__manifest__.py
@@ -2,7 +2,7 @@
 # Copyright 2021 Alejandro Olano <Github@alejo-code>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Product Archive Category",
+    "name": "Product Category Archive Security",
     "version": "10.0.1.0.0",
     "author": "EXA Auto Parts Github@exaap, Alejandro Olano Github@alejo-code",
     "license": "AGPL-3",
