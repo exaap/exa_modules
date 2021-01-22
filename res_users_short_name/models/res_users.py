887854b2edd11a45e709f3fb9cd200ae300ab8d9
Alejandro Olano <alejocode@gmail.com>
2021-01-22 16:54:01 -0500

    [ADD] Res Users Short Name

---
+++ res_users_short_name/models/res_users.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import models, fields
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+    short_name = fields.Char(string="Short Name", size=7)
