3d06a5caae41d3304c65bf27df8ff9fd1a4d6653
Alejandro Olano <alejocode@gmail.com>
2021-01-28 16:04:13 -0500

    [ADD] Exa Partner Non Territorial (#102)

---
+++ exa_partner_non_territorial/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 EXA Auto Parts S.A.S Joan Marín <Github@JoanMarin>
+# Copyright 2021 EXA Auto Parts S.A.S Alejandro Olano <Github@Alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import models
---
+++ exa_partner_non_territorial/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 EXA Auto Parts S.A.S Joan Marín <Github@JoanMarin>
+# Copyright 2021 EXA Auto Parts S.A.S Alejandro Olano <Github@Alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Partner Non-territorial",
+    "summary": "Partner Non-territorial",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/exaap/exaap_modules",
+    "author": "EXA Auto Parts Github@exaap, "
+    "Joan Marín Github@JoanMarin",
+    "category": "EXA Modules",
+    "depends": ["base"],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+}
