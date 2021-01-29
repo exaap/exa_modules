fffb70bd7ee49b09ac371c7e4c192ceb71ddaeae
Alejandro Olano <alejocode@gmail.com>
2021-01-29 13:41:04 -0500

    [ADD] Account Invoice  Partner Country (#104)

---
+++ account_invoice_partner_country/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Invoice Partner Country",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/exaap/exaap_modules",
+    "author": "EXA Auto Parts Github@exaap, Alejandro Olano Github@alejo-code",
+    "depends": ["base", "account"],
+    "data": [
+        "views/account_invoice_view.xml",
+    ],
+    "installable": True,
+}
