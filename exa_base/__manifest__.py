d661b14978488b2abeeb5cda0f3f2e7133fa8bd6
Joan Marín <joanmarinbustamante@gmail.com>
2021-02-02 15:22:37 -0500

    [ADD] exa_base and exa_website_payment modules (#109)

---
+++ exa_base/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "EXA Base",
+    "author": "EXA Auto Parts Github@exaap, "
+              "Joan Marín Github@JoanMarin",
+    "license": "AGPL-3",
+    "category": "EXA Modules",
+    "version": "10.0.1.0.0",
+    "website": "https://github.com/exaap/exaap_modules",
+    "installable": True,
+    "images": [],
+    "depends": [
+        "exa_base_modules",
+    ],
+    "data": [
+        "security/res_groups_security.xml",
+    ],
+}
