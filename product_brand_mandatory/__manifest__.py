b90b8bb721590328da58a3b35eb2aca80090805d
Alejandro Olano <alejocode@gmail.com>
2021-02-12 15:53:35 -0500

    [ADD] Product Brand Mandatory (#129)

---
+++ product_brand_mandatory/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Product Brand Mandatory",
+    "license": "AGPL-3",
+    "author": "EXA Auto Parts Github@exaap, Alejandro Olano Github@alejo-code",
+    "version": "11.0.1.0.0",
+    "website": "https://github.com/exaap/exaap_modules",
+    "depends": ['sale', 'product_brand'],
+    "data": [
+        'views/product_template.xml',
+    ]
+}
