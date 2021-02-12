94fdc2e6e3bc1e7017d1815f6e51a3af01c12f15
Alejandro Olano <alejocode@gmail.com>
2021-02-12 13:43:50 -0500

    [ADD] Product Applications

---
+++ product_application/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name":
+    "Product Application",
+    "license":
+    "AGPL-3",
+    "author":
+    "EXA Auto Parts Github@exaap, Alejandro Olano Github@alejo-code",
+    'category':
+    'Warehouse',
+    "version":
+    "11.0.1.0.0",
+    "website":
+    "https://github.com/exaap/exaap_modules",
+    "depends": ['sale'],
+    "data": [
+        'security/groups.xml', 'security/ir.model.access.csv',
+        'views/product_template.xml', 'views/product_application.xml',
+        'views/product_application_category.xml', 'views/product_product.xml'
+    ]
+}
