2faf905509c7a74441c485fd346dcb4bb6b3d426
Alejandro Olano <alejocode@gmail.com>
2021-02-17 08:29:18 -0500

    [ADD] Exa Base Modules

---
+++ exa_base_modules/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Joan Marín <Github@JoanMarin>
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Base for EXA Modules",
+    "author": "EXA Auto Parts Github@exaap, "
+    "Joan Marín Github@JoanMarin, "
+    "Alejandro Olano Github@alejo-code",
+    "license": "AGPL-3",
+    "category": "EXA Modules",
+    "version": "12.0.1.0.0",
+    "website": "https://github.com/exaap/exaap_modules",
+    "installable": True,
+    "images": [],
+    "depends": ["base"],
+    "data": [
+        "data/ir_module_category_data.xml",
+    ],
+}
