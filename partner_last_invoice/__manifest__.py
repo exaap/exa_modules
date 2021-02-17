4f9c330f93c8cfc6ec6a7a3b15e8aafa2ca9faed
Alejandro Olano <alejocode@gmail.com>
2021-02-17 15:55:44 -0500

    [ADD] Partner Last Invoice (#143)

---
+++ partner_last_invoice/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Joan Marín <Github@JoanMarin>
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Partner Last Invoice",
+    "author": "EXA Auto Parts Github@exaap, "
+    "Joan Marín Github@JoanMarin, "
+    "Alejandro Olano Github@alejo-code",
+    "license": "AGPL-3",
+    "category": "EXA Modules",
+    "version": "11.0.1.0.0",
+    "website": "https://github.com/exaap/exaap_modules",
+    "installable": True,
+    "images": [],
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+}
