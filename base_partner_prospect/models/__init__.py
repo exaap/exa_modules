be55200bc9eda04897980109a80926c37eb352f5
Alejandro Olano <alejocode@gmail.com>
2021-02-17 08:41:21 -0500

    [ADD] Base Partner Prospect

---
+++ base_partner_prospect/models/__init__.py
@@ -0,0 +1,4 @@
+# © 2015-2016 Akretion (Alexis de Lattre <alexis.delattre@akretion.com>)
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import res_partner
