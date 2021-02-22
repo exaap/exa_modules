e212cf6ffaabd6bf0846f6bf17c51963bcf7ee18
Alejandro Olano <alejocode@gmail.com>
2021-02-22 11:35:00 -0500

    [ADD] Exa Account Invoice Line Quantity Signed

---
+++ exa_account_invoice_line_quantity_signed/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Joan Marín <Github@JoanMarin>
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Quantity Signed in Invoice Line",
+    "author": "EXA Auto Parts Github@exaap, "
+    "Joan Marín Github@JoanMarin, "
+    "Alejandro Olano Github@alejo-code",
+    "license": "AGPL-3",
+    "category": "EXA Modules",
+    "version": "12.0.1.0.0",
+    "website": "https://github.com/exaap/exaap_modules",
+    "installable": True,
+    "images": [],
+    "depends": [
+        "exa_base_modules",
+        "account",
+    ],
+    "data": [
+        "views/account_invoice_views.xml",
+    ],
+}
