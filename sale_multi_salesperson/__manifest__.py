717b7e5ff26ffdd9acc1b72861dfa351510877d5
Joan Marín <joanmarinbustamante@gmail.com>
2021-02-24 14:24:50 -0500

    [ADD] sale_multi_salesperson module (#151)

---
+++ sale_multi_salesperson/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Multiple Sales Person for a Customer",
+    "summary": "Multiple sales people for a customer (Sale Orders / Invoice).",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://www.exaap.com",
+    "author": "EXA Auto Parts Github@exaap, "
+              "Joan Marín Github@JoanMarin",
+    "category": "Sale",
+    "depends": [
+        "sale"
+    ],
+    "data": [
+        "views/res_partner_views.xml",
+        "views/sale_order_views.xml", 
+        "views/account_invoice_views.xml"
+    ],
+    "installable": True,
+}
