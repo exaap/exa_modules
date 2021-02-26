d2a63ecf80737933413ea06097666a1c7f5666ba
Alejandro Olano <alejocode@gmail.com>
2021-02-26 14:42:43 -0500

    [ADD] Exa Sale Margin

---
+++ exa_sale_margin/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": " Exa Sale Order Margin Percent",
+    "summary": "Show Percent in sale order",
+    "version": "12.0.1.0.0",
+    "category": "Sales",
+    "author": "EXA Auto Parts Github@exaap, Alejandro Olano Github@alejo-code",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ['sale_order_margin_percent'],
+    "data": [
+        'views/sale_margin_percent_views.xml',
+    ]
+}
