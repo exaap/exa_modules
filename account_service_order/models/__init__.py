be4c85f554da3d1e82eedd1360188315cf903c18
Alejandro Olano <alejocode@gmail.com>
2021-03-10 14:04:10 -0500

    [ADD] Account Service Order (#168)

---
+++ account_service_order/models/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Joan Marín <Github@JoanMarin>
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3
+from . import account_account
+from . import account_invoice_line
+from . import account_invoice
+from . import account_move_line
+from . import account_service_order
