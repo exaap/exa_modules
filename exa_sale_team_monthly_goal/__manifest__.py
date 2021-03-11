53d1c39cf53d7fcfc89e960e729c9392c91c02d7
Alejandro Olano <alejocode@gmail.com>
2021-03-11 12:53:12 -0500

    [ADD] Exa Sale Team Monthly Goal (#169)

---
+++ exa_sale_team_monthly_goal/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    'name': " Exa Sale Team Monthly Goal",
+    'summary': """
+        This module add a monthly goal for the sale_team           
+    """,
+    'description': """
+        This module add a monthly goal for the sale_team
+    """,
+    'author':
+    "EXA Auto Parts S.A.S & Solutions, Alejandro Olano Github@alejo-code",
+    'website': "https://github.com/exaap/exaap_modules",
+    'category': 'Addon',
+    'version': '11.0.0.2',
+    'depends': ['crm', 'sale_management'],
+    'data': ['views/crm_team_form_view.xml'],
+}
