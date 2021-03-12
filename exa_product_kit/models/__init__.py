bc22e64a342454d3cfd9feb0147ac4263a22b0b8
Alejandro Olano <alejocode@gmail.com>
2021-03-12 14:59:05 -0500

    [ADD] Exa Product Kit

---
+++ exa_product_kit/models/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Joan Marín <Github@JoanMarin>
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import account_invoice_line
+from . import procurement_rule
+from . import product_kit_line
+from . import product_template
+from . import sale_order
+from . import sale_order_kit_line
+from . import sale_order_line
+from . import stock_move
