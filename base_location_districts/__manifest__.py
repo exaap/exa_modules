a43729e2b6da4e47a6a340f4ea38d34540bb6277
Alejandro Olano <alejocode@gmail.com>
2021-03-12 12:04:32 -0500

    [ADD] Base Location Districts

---
+++ base_location_districts/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name':
+    'Base Location Districts',
+    'version':
+    '11.0.1.1.0',
+    'depends': ['base', 'base_location'],
+    'author':
+    "EXA Auto Parts S.A.S - Alejandro Olano Github@alejo-code",
+    'license':
+    "AGPL-3",
+    'summary':
+    '''Enhanced zip/city with district data.''',
+    'data':
+    ['views/res_partner_view.xml', 'views/res_better_zip_district_view.xml'],
+    'installable':
+    True,
+    'auto_install':
+    False,
+}
