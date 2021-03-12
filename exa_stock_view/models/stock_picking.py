8d46b495a13163405161f82207f87e6a1e3417c2
Alejandro Olano <alejocode@gmail.com>
2021-03-12 15:24:26 -0500

    [ADD] Exa Stock View

---
+++ exa_stock_view/models/stock_picking.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Joan Marín <Github@JoanMarin>
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    partner_street = fields.Char(related='partner_id.street')
+    partner_district_id = fields.Many2one(related='partner_id.district_id')
+    partner_zone_id = fields.Many2one(related='partner_id.zone_id')
+    partner_zip_id = fields.Many2one(related='partner_id.zip_id')
