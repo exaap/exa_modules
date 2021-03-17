3ddae50a1161abdb2cfd45764304b7e4f43daf79
Alejandro Olano <alejocode@gmail.com>
2021-03-17 09:45:19 -0500

    [ADD] Account Delivery (#187)

---
+++ account_delivery/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Joan Marín <Github@JoanMarin>
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Delivery",
+    "author": "EXA Auto Parts Github@exaap, "
+    "Joan Marín Github@JoanMarin, "
+    "Alejandro Olano Github@alejo-code",
+    "license": "AGPL-3",
+    "category": "EXA Modules",
+    "version": "12.0.1.0.0",
+    "website": "https://github.com/exaap/exaap_modules",
+    "installable": True,
+    "images": [],
+    "depends": [
+        "account",
+        "delivery",
+    ],
+    "data": [
+        "views/account_invoice_views.xml",
+    ],
+}
