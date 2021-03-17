54c2b822ec0d26ab5c830cd388175120d54cee4a
Alejandro Olano <alejocode@gmail.com>
2021-03-17 11:11:33 -0500

    [ADD] Base Location Zones (#189)

---
+++ base_location_zones/models/res_city_zip_district.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import models, fields, api
+
+
+class ResCityZipDistrict(models.Model):
+    _inherit = 'res.city.zip.district'
+
+    zone_id = fields.Many2one(comodel_name='res.city.zip.zone',
+                              string='Zone')
