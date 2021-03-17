a116dc81ce447e58ba95893e0faf48a672972d0f
Alejandro Olano <alejocode@gmail.com>
2021-03-17 09:20:01 -0500

    [ADD] Partner Birth And Fundation Dates (#185)

---
+++ partner_birth_and_foundation_dates/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 EXA Auto Parts S.A.S Joan Marín <Github@joanmarin>
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Birth and Foundation Dates for Partner",
+    "summary": "Birth and Foundation Dates for Partner",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/exaap/exaap_modules",
+    "author": "EXA Auto Parts Github@exaap, "
+    "Joan Marín Github@joanmarin, "
+    "Alejandro Olano Github@alejo-code",
+    "category": "Partner Management",
+    "depends": ["base"],
+    "data": [
+        "views/res_partner_views.xml",
+    ],
+    "installable": True,
+}
