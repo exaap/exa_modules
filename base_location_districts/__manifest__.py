7bdd2d6bc3b82c2b0326ce81ac21d41a560660f6
Alejandro Olano <alejocode@gmail.com>
2021-04-14 09:21:17 -0500

    [FIX] Base Location Districts

--- base_location_districts/__manifest__.py
+++ base_location_districts/__manifest__.py
@@ -3,14 +3,23 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Base Location Districts',
-    'version': '12.0.1.1.0',
+    'name':
+    'Base Location Districts',
+    'version':
+    '12.0.1.1.0',
     'depends': ['base', 'base_location'],
-    'author': "EXA Auto Parts S.A.S - Alejandro Olano Github@alejo-code",
-    'license': "AGPL-3",
-    'summary': '''Enhanced zip/city with district data.''',
-    'data':
-    ['views/res_partner_view.xml', 'views/res_city_zip_district_view.xml'],
-    'installable': True,
-    'auto_install': False,
+    'author':
+    "EXA Auto Parts S.A.S - Alejandro Olano Github@alejo-code",
+    'license':
+    "AGPL-3",
+    'summary':
+    '''Enhanced zip/city with district data.''',
+    'data': [
+        'security/ir.model.access.csv', 'views/res_partner_view.xml',
+        'views/res_city_zip_district_view.xml'
+    ],
+    'installable':
+    True,
+    'auto_install':
+    False,
 }
