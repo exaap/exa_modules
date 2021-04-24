d69fb523e761ad0bc1bf926c0ab2da25203c3a40
Alejandro Olano <alejocode@gmail.com>
2021-04-24 10:43:19 -0500

    [ADD] Stock of Main Locations (#209)

---
+++ product_main_locations/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Joan Marín <Github@joanmarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock of Main Locations",
+    "category": "Stock",
+    "version": "10.0.1.0.0",
+    "author": "EXA Auto Parts Github@exaap, "
+              "Joan Marín Github@joanmarin",
+    "website": "http://www.exaap.com",
+    "license": "AGPL-3",
+    "summary": "Stock of Main Locations", 
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/product_product_views.xml",
+        "views/product_template_views.xml",
+        "views/stock_location_views.xml",
+    ],
+    "installable": True,
+}
---
+++ product_main_locations/models/stock_location.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Joan Marín <Github@joanmarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class StockLocation(models.Model):
+    _inherit = 'stock.location'
+
+    is_main = fields.Boolean(string='Is Main?', default=False)
