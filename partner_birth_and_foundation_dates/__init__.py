ccc9370f3ec8ed58441e9e87ad22eb5e005eb5cb
Alejandro Olano <alejocode@gmail.com>
2021-04-24 09:17:15 -0500

    [ADD] Birth and Foundation Dates for Partner (#207)

---
+++ partner_birth_and_foundation_dates/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 EXA Auto Parts S.A.S Joan Marín <Github@joanmarin> 
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import models
