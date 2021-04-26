84d454458e14dbd0d04af7cd7cce612b4696006d
Alejandro <alejocode@gmail.com>
2021-04-26 10:15:43 -0500

    [ADD] Product Inventory Lines

---
+++ product_stock_inventory_line/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Joan Marín <Github@JoanMarin>
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Inventory Lines",
+    "version": "11.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "EXA Auto Parts Github@exaap, "
+              "Joan Marín Github@JoanMarin, "
+              "Alejandro Olano Github@alejo-code",
+    "website": "https://github.com/exaap/exaap_modules",
+    "category": "Product",
+    "depends": [
+        "product",
+        "stock"
+    ],
+    "data": [
+        "views/product_template_views.xml"
+    ],
+    "installable": True,
+}
---
+++ product_stock_inventory_line/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Joan Marín <Github@JoanMarin>
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import stock_inventoy_line
+from . import product_product
+from . import product_template
