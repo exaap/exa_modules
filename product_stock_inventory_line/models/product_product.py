f1e9e3e18fee30b22dcfcb5a5ebf7cf51fea40ff
Alejandro <alejocode@gmail.com>
2021-04-26 10:24:05 -0500

    [ADD] Product Inventory Lines

---
+++ product_stock_inventory_line/models/product_product.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Joan Marín <Github@JoanMarin>
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class ProductProduct(models.Model):
+    _inherit = "product.product"
+
+    inventory_line_ids = fields.One2many(comodel_name='stock.inventory.line',
+                                         inverse_name='product_id',
+                                         string='Inventory Lines')
