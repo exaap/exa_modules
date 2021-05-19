6ce33b4e85f1afc8a4832bad88ff2b75b926df9b
Alejandro Olano <alejocode@gmail.com>
2021-05-19 10:27:01 -0500

    [ADD] Account Archive Security (#223)

---
+++ account_account_archive_security/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Archive Security",
+    "license": "AGPL-3",
+    "author": "EXA Auto Parts Github@exaap, Alejandro Olano Github@alejo-code",
+    "version": "11.0.1.0.0",
+    "website": "https://github.com/exaap/exaap_modules",
+    "depends": ["account"],
+    "data": ["views/account_view.xml"]
+}
