98532223f87fb037059ad3a1089f7473f97dfd37
Alejandro Olano <alejocode@gmail.com>
2021-05-19 17:33:08 -0500

    [ADD] Product Service Security (#231)

---
+++ product_service_security/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Service Security",
+    "license": "AGPL-3",
+    "author": "EXA Auto Parts Github@exaap, Alejandro Olano Github@alejo-code",
+    "version": "12.0.1.0.0",
+    "website": "https://github.com/exaap/exaap_modules",
+    "depends": ["product"],
+    "installable": True,
+    "data": ["security/groups.xml"]
+}
