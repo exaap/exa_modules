e0f901cf463487263019587e4facd539c613b235
Alejandro Olano <alejocode@gmail.com>
2021-05-19 18:05:23 -0500

    [ADD] Product Category Archive Security

---
+++ product_category_archive_security/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Category Archive Security",
+    "version": "12.0.1.0.0",
+    "author": "EXA Auto Parts Github@exaap, Alejandro Olano Github@alejo-code",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ['product'],
+    "data": ["views/product_category.xml"]
+}
