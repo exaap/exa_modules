5b97c7583513c659f9ddd4ba95884e6fc9bd1cb5
Alejandro Olano <alejocode@gmail.com>
2021-05-18 22:25:42 -0500

    [ADD] Custom Website Fonts

---
+++ website_fonts/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Custom Website Fonts',
+    'author': "EXA Auto Parts Github@exaap, Alejandro Olano Github@alejo-code",
+    'depends': ['website'],
+    'data': [
+        'views/website_assets_views.xml',
+    ],
+}
