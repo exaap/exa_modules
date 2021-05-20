b123aef83934487bb00c2814449a0851fed2efd0
Alejandro Olano <alejocode@gmail.com>
2021-05-20 15:19:32 -0500

    [ADD] Product Volume Precision

---
+++ product_volume_precision/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Volume Precision",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "EXA Auto Parts Github@exaap, "
+              "Joan Marín Github@JoanMarin",
+    "website": "https://github.com/exaap/exaap_modules",
+    "category": "Product",
+    "depends": [
+        "product",
+    ],
+    "data": [
+        "data/decimal_precision_data.xml"
+    ],
+    "installable": True,
+}
