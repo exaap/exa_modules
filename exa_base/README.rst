8094f82b67480e99909aca78b03de90d672f9a6b
Alejandro Olano <alejocode@gmail.com>
2021-05-22 11:47:05 -0500

    [ADD] EXA Base

---
+++ exa_base/README.rst
@@ -0,0 +1,13 @@
+.. image:: https://img.shields.io/badge/license-AGPL--3-blue.png
+   :target: https://www.gnu.org/licenses/agpl
+   :alt: License: AGPL-3
+
+========
+EXA Base
+========
+
+Contributors
+------------
+
+* Alejandro Olano <https://github.com/alejo-code>
+* Joan Marín <https://github.com/JoanMarin>
---
+++ exa_base/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Joan Marín <Github@JoanMarin>
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "EXA Base",
+    "author":
+    "EXA Auto Parts Github@exaap, Alejandro Olano Github@alejo-code, "
+    "Joan Marín Github@JoanMarin",
+    "license": "AGPL-3",
+    "category": "EXA Modules",
+    "version": "12.0.1.0.0",
+    "website": "https://github.com/exaap/exaap_modules",
+    "installable": True,
+    "images": [],
+    "depends": [
+        "exa_base_modules",
+    ],
+    "data": [
+        "security/res_groups_security.xml",
+    ],
+}
