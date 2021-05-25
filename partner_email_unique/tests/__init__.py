8b717b21792ad1f51f45ef0c11361831e0efcc32
Alejandro Olano <alejocode@gmail.com>
2021-05-25 11:37:34 -0500

    [ADD] Partner unique email (#252)

---
+++ partner_email_unique/tests/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Coop IT Easy SCRL fs
+#   Robin Keunen <robin@coopiteasy.be>
+#  Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from . import test_res_partner_email
