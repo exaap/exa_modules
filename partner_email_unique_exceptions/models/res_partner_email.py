040762ca00ad1daa0d316c67d6429ccca82ceb85
Alejandro Olano <alejocode@gmail.com>
2021-05-25 11:47:39 -0500

    [ADD] Partner unique email Exceptions (#253)

---
+++ partner_email_unique_exceptions/models/res_partner_email.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#  Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import models, fields
+
+
+class PartnerEmailException(models.Model):
+    _name = "res.partner.email.exception"
+
+    name = fields.Char(string="Email Exceptions")
