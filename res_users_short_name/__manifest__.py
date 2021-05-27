973494f34f7550e413a830f52cbb032ab75630c1
Alejandro Olano <alejocode@gmail.com>
2021-05-27 16:12:31 -0500

    [ADD] Res Users Short Name

---
+++ res_users_short_name/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Res Users Short Name",
+    "license": "AGPL-3",
+    "author": "EXA Auto Parts Github@exaap, Alejandro Olano Github@alejo-code",
+    "version": "12.0.1.0.0",
+    "website": "https://github.com/exaap/exaap_modules",
+    "depends": ['base', 'partner_second_lastname'],
+    "data": ['views/res_users_view.xml']
+}
