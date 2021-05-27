cdbe21587edcd2444bbb66b3d0fb012041a1286a
Alejandro Olano <alejocode@gmail.com>
2021-05-27 16:33:11 -0500

    [ADD] EXA Payment: Website Integration (#274)

---
+++ exa_website_payment/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "EXA Payment: Website Integration",
+    "author": "EXA Auto Parts Github@exaap, "
+              "Joan Marín Github@JoanMarin",
+    "license": "AGPL-3",
+    "category": "EXA Modules",
+    "version": "12.0.1.0.0",
+    "website": "https://github.com/exaap/exaap_modules",
+    "images": [],
+    "depends": [
+        "exa_base",
+        "website_payment",
+    ],
+    "data": [],
+    "installable": True,
+    'auto-install': True,
+}
