c4295187385d4831753881b90d85668573acec70
Alejandro Olano <alejocode@gmail.com>
2021-05-27 16:24:28 -0500

    [ADD] Allow Edit Risk in Partner (#273)

---
+++ partner_risk_allow_edit_security/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Allow Edit Risk in Partner",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "EXA Auto Parts Github@exaap, "
+              "Joan Marín Github@JoanMarin",
+    "website": "https://github.com/exaap/exaap_modules",
+    "category": "Sales Management",
+    "depends": [
+        "account_financial_risk",
+    ],
+    "data": [
+        "security/res_partner_security.xml",
+    ],
+    "installable": True,
+}
