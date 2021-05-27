1c440c4661b97a297bef16800f5ad8adce7a4e80
Alejandro Olano <alejocode@gmail.com>
2021-05-27 18:01:42 -0500

    [ADD] Best def create() Sale Sourced by Line

---
+++ sale_sourced_by_line_def_create/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Best def create() Sale Sourced by Line',
+    'version': '1.0',
+    'category': 'Sale',
+    'sequence': 10,
+    'summary': 'Best def create() Sale Sourced by Line',
+    'description': """
+            Best def create() in model sale.order.line for Sale Sourced by Line
+            """,
+    'author': "EXA Auto Parts Github@exaap, Alejandro Olano Github@alejo-code",
+    'website': 'https://www.exaap.com',
+    'depends': ['sale_sourced_by_line'],
+    'data': [],
+    'demo': [],
+    'images': [],
+    'css': [],
+    'installable': True,
+}
