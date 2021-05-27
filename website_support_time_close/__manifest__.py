0368600df358e3cb7ee0c270e07d8a2ef7a7cee4
Alejandro Olano <alejocode@gmail.com>
2021-05-27 15:56:48 -0500

    [ADD] Website Support Time Close

---
+++ website_support_time_close/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Website Support Time Close",
+    "license": "AGPL-3",
+    "author": "EXA Auto Parts Github@exaap, Alejandro Olano Github@alejo-code",
+    'category': 'Website',
+    "version": "12.0.1.0.0",
+    "website": "https://github.com/exaap/exaap_modules",
+    "depends": ['website_support'],
+    "data": ["views/website_support_tiket_view.xml"]
+}
---
+++ website_support_time_close/models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import website_support_ticket
+from . import website_support_ticket_close
