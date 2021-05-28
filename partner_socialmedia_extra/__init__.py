125c88f1040a6036e651c9705b3cd4bb2d69bad1
Alejandro Olano <alejocode@gmail.com>
2021-05-27 22:36:45 -0500

    [ADD] Partner Socialmedia Extra

---
+++ partner_socialmedia_extra/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Humanitarian Logistics Organisation e.V. - Stefan Becker
+# Copyright 2020 Joan Marín <Github@JoanMarin>
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from . import models
