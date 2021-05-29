70893bcd7fd60bba1dc5d73fc2013a7c4a43813a
Alejandro Olano <alejocode@gmail.com>
2021-05-29 10:34:03 -0500

    [ADD] Product Multiple Barcodes (#287)

---
+++ product_multi_barcode/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Juan Sebastian Ocampo Ospina <Github@Capriatto>
+# Copyright 2020 Joan Marín <Github@JoanMarin>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import product_barcode
+from . import product_template
+from . import product_product
