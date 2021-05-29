f6f790cbf03ca5f71131cad2f67f5b1bc809f9aa
Alejandro Olano <alejocode@gmail.com>
2021-05-29 10:45:24 -0500

    [FIX] Project Department Categorization (#289)

--- project_department/models/project.py
+++ project_department/models/project.py
@@ -1,27 +1,22 @@
-# © 2014 Joël Grand-Guillaume (Camptocamp)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# -*- coding: utf-8 -*-
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from odoo import fields, models
 
 
 class ProjectTask(models.Model):
     _inherit = 'project.task'
 
-    project_department_id = fields.Many2one(
-        related='project_id.department_id',
-        string='Project Department',
-        store=True
-    )
+    project_department_id = fields.Many2one(related='project_id.department_id',
+                                            string='Project Department',
+                                            store=True)
 
 
 class Project(models.Model):
-    _name = "project.project"
-    _description = "Project"
-    _inherit = ['mail.alias.mixin', 'mail.thread', 'portal.mixin']
-    _inherits = {'account.analytic.account': "analytic_account_id"}
+    _inherit = "project.project"
 
-    analytic_account_id = fields.Many2one(
-        'account.analytic.account',
-        string='Analytic Account',
-        required=True,
-        ondelete='restrict'
-    )
+    department_id = fields.Many2one(
+        related='analytic_account_id.department_id',
+        string='Project Department',
+        readonly=False)
