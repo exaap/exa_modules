f137ee9ff5ba0106428e2d4669fae59e393d2f5b
Alejandro Olano <alejocode@gmail.com>
2021-05-29 10:44:22 -0500

    [FIX] Project Department Categorization

--- project_department/__init__.py
+++ project_department/__init__.py
@@ -1,2 +1,5 @@
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# -*- coding: utf-8 -*-
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 from . import models
--- project_department/__manifest__.py
+++ project_department/__manifest__.py
@@ -1,17 +1,16 @@
-# Copyright 2014 Joël Grand-Guillaume (Camptocamp)
+# -*- coding: utf-8 -*-
+# Copyright 2021 Alejandro Olano <Github@alejo-code>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
 {
     "name": "Project Department Categorization",
     "version": "12.0.1.0.0",
-    "author": "Camptocamp, Daniel Reis, Odoo Community Association (OCA)",
+    "author": "EXA Auto Parts Github@exaap, Alejandro Olano Github@alejo-code",
     "license": "AGPL-3",
     "category": "Generic Modules/Projects & Services",
-    "website": "https://github.com/OCA/project",
     "depends": [
         "analytic_base_department",
         "project",
     ],
-    "data": [
-        "views/project.xml"
-    ],
+    "data": ["views/project.xml"],
 }
